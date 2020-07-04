d45a8cc3af3e02795b2df95ed70de98ef0c44a94
juveria-manzar <juveriadot@gmail.com>
2020-07-04 17:37:39 +0530

    removing-item

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -17,4 +17,7 @@ export class ListManagerComponent implements OnInit {
   addItem(title: string) {
     this.todoListService.addItem({ title });
   }
+  removeItem(item) {
+  this.todoListService.deleteItem(item);
+}
 }
